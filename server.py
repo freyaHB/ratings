068f1bd8f351bd271bf64b1736c6d0b2bb374c47
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-05 13:03:02 -0700

    Next Commit with Flask

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -16,13 +16,45 @@ app.secret_key = "ABC"
 # Normally, if you use an undefined variable in Jinja2, it fails
 # silently. This is horrible. Fix this so that, instead, it raises an
 # error.
-app.jinja_env.undefined = StrictUndefined
+app.jinja_env.undefined = StrictUndefined(name='foo')
 
 
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route('/register', methods=["GET"])
+def register_form():
+    return render_template("register_form.html")
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    user_email = request.form.get("email")
+    password = request.form.get("password")
+    query = User.query.filter(User.email == user_email).first()
+    
+    if query is None:
+        new_user = User(email=user_email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+    return redirect("/")
+
+    
+
+            
+
+
+
+
+    
 
 
 if __name__ == "__main__":
